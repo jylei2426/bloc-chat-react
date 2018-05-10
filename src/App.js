1f1b96b3c6462e4202c94ce0ec16095abab27220
Jenny Lei <jylei2426@live.carlow.edu>
2018-05-09 21:55:16 -0400

    add changes to list chat

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAy3363QOkvnPiOLhjxHtYJsHkbzVdD_4U",
+    authDomain: "bloc-chat-fe5c7.firebaseapp.com",
+    databaseURL: "https://bloc-chat-fe5c7.firebaseio.com",
+    projectId: "bloc-chat-fe5c7",
+    storageBucket: "bloc-chat-fe5c7.appspot.com",
+    messagingSenderId: "17869437316"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Bloc Chat </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      
       </div>
     );
   }
