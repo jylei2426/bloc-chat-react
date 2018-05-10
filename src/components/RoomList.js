6d5b3b379732d62c3ddc16ddc3f17caeb9c471b5
Jenny Lei <jylei2426@live.carlow.edu>
2018-05-10 00:01:01 -0400

    list chat room checkpoint

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,35 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react'
 
 class RoomList extends Component {
-  constructor(props) {
-    super(props);
+   constructor(props) {
+      super(props);
+      this.state = {
+         rooms: []
+      };
+      this.roomsRef = this.props.firebase.database().ref('rooms');
+   }
 
-    this.state = {
-    rooms: [],
-    newRoomName: "",
-  };
-  this.roomsRef = this.props.firebase.database().ref('rooms');
-  }
+   componentDidMount() {
+      this.roomsRef.on('child_added', snapshot => {
+         const room = snapshot.val();
+         room.key = snapshot.key;
+         this.setState({ rooms: this.state.rooms.concat(room) });
+      });
+   }
 
-  componentDidMount() {
-       this.roomsRef.on('child_added', snapshot => {
-         console.log(snapshot);
-       });
-     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   render() {
+      return (
+         <div>
+            {
+               this.state.rooms.map( (room, index) =>
+                  <div key={room.key}>
+                     {room.name}
+                  </div>
+               )
+            }
+         </div>
+      )
+   }
+}
 
 export default RoomList;
