41f158a0cbc90521271f47ca571ab1156d13a17c
Jenny Lei <jylei2426@live.carlow.edu>
2018-05-10 22:27:35 -0400

    create chat rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -11,22 +11,41 @@ class RoomList extends Component {
 
    componentDidMount() {
       this.roomsRef.on('child_added', snapshot => {
+         //console.log(snapshot);
          const room = snapshot.val();
          room.key = snapshot.key;
          this.setState({ rooms: this.state.rooms.concat(room) });
       });
    }
 
+   createRoom(e){
+     e.preventDefault();
+     console.log("create room")
+     this.roomsRef.push({
+     name: this.state.newRoomName
+     });
+   }
+
+   handleChange(e){
+     this.setState({newRoomName: e.target.value})
+   }
+
+
    render() {
       return (
          <div>
             {
-               this.state.rooms.map( (room, index) =>
-                  <div key={room.key}>
-                     {room.name}
-                  </div>
-               )
-            }
+              this.state.rooms.map( (room, index) =>
+              <div key={room.key}>
+                      {room.name}
+        </div>
+            )
+           }
+          <form onSubmit={ (e) => this.createRoom(e)}>
+          <input type= "text" onChange={this.handleChange} value={this.state.newRoomName} />
+          <input type= "submit" />
+          </form>
+
          </div>
       )
    }
