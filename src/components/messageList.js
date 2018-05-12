5845fdfed1c2bd71c2a9f23452d99c5b0458bc2a
Jenny Lei <jylei2426@live.carlow.edu>
2018-05-12 00:53:48 -0400

    list message checkpoint

---
+++ src/components/messageList.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+class MessageList extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         messages: []
+      };
+      this.messagesRef = this.props.firebase.database().ref('messages');
+   }
+
+   componentDidMount() {
+      this.messagesRef.on('child_added', snapshot => {
+         //console.log(snapshot);
+         const message = snapshot.val();
+         message.key = snapshot.key;
+         this.setState({ rooms: this.state.messages.concat(message) });
+      });
+   }
+
+   render() {
+          return (
+        <section className='MessageList' >
+          <table id="messages-sent">
+            <thead>
+              <tr>
+                <th>{this.props.activeRoomName}</th>
+              </tr>
+            </thead>
+            <colgroup>
+              <col id ="username-column"/>
+              <col id ="time-sent-column"/>
+              <col id ="content-column"/>
+            </colgroup>
+            <tbody>
+              { this.state.messages.map((message,index) =>
+                message.roomId === this.props.activeRoom &&
+                <tr className="message-data" key={index}>
+                  <td className="username">{message.username}</td>
+                  <td className="time-sent">{message.sentAt}</td>
+                  <td className="content">{message.content}</td>
+                </tr>
+              )
+            }
+            </tbody>
+          </table>
+        </section>);
+      }
+  }
+
+export default MessageList;
